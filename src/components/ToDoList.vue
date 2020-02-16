d65e04ada67751af18da3994f5c1e21a0cec949d
kijong jo <cozacoza12@naver.com>
2020-02-16 14:46:43 +0900

    Feat
    ToDoList 컴포넌트에 로컬 데이터 출력하는 기능 추가

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,11 +1,41 @@
 <template lang="html">
-  <div>
-  list
-  </div>
+   <section>
+     <ul>
+       <li v-for="todoItem in todoItems">{{todoItem}}</li>
+
+     </ul>
+   </section>
+
 </template>
 
 <script>
 export default {
+  data() {
+
+    return {
+      //배열로 선언
+      todoItems: []
+
+    }
+
+  },
+  //뷰의 인스턴스가 생성되자마자 뷰 데이터에 접근 할 수 있도록 created() 라이프 사이클 훅에서 데이터를 뷰 데이터로 옮긴다.
+  created() {
+    //저장된 데이터 값이 0이상일 경우 반복문 실행
+    if (localStorage.length > 0) {
+
+      for (var i = 0; i < localStorage.length; i++) {
+        //todoItems 배열에 값을 집어 넣는다.
+        // push는 배열의 끝 요소에 배열 아이템을 하나씩 추가하는 자바스크립트 내장 API이다.
+        this.todoItems.push(localStorage.key(i));
+      }
+
+
+
+    }
+
+  }
+
 }
 </script>
 
