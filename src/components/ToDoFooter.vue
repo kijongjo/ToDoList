bdba70dca497f9c0bcfed54c314de05bc243eb07
kijong jo <cozacoza12@naver.com>
2020-02-16 21:31:58 +0900

    Feat
    할 일 목록 모두 지우기 기능 추가

--- src/components/ToDoFooter.vue
+++ src/components/ToDoFooter.vue
@@ -1,13 +1,34 @@
 <template lang="html">
-  <div >
-  footer
+  <div class="clearAllContainer" >
+  <span class="clearAllBtn" @click="clearTodo">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
+  methods:{
+    clearTodo(){
+     localStorage.clear();
+
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+.clearAllContainer{
+     width:8.5rem;
+     height:50px;
+     line-height:50px;
+     background-color:white;
+     border-radius:5px;
+     margin:0 auto;
+
+}
+.clearAllBtn{
+  color:#e20303;
+  display:black;
+
+
+}
 </style>
